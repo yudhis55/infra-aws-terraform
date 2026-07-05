51169c1cb28b2cb5205011551c3f16118f0f2e97
yudhis55 <yudisputra55@gmail.com>
2026-07-05 21:23:25 +0700

    Enable ECS container insights

--- modules/ecs/main.tf
+++ modules/ecs/main.tf
@@ -1,6 +1,11 @@
 resource "aws_ecs_cluster" "main" {
   name = "${var.project_name}-cluster"
 
+  setting {
+    name  = "containerInsights"
+    value = "enabled"
+  }
+
   tags = {
     Name = "${var.project_name}-cluster"
   }
