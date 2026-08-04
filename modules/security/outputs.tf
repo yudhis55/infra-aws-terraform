8d60855e4f842a114eb32adc8d8851af5dc9bb77
yudhis55 <yudisputra55@gmail.com>
2026-08-04 07:12:32 +0700

    fix: tolerate partial Terraform state in outputs

--- modules/security/outputs.tf
+++ modules/security/outputs.tf
@@ -1,31 +1,31 @@
 output "waf_web_acl_arn" {
   description = "ARN of the WAF Web ACL"
-  value       = var.enable_waf ? aws_wafv2_web_acl.main[0].arn : null
+  value       = try(aws_wafv2_web_acl.main[0].arn, null)
 }
 
 output "waf_web_acl_id" {
   description = "ID of the WAF Web ACL"
-  value       = var.enable_waf ? aws_wafv2_web_acl.main[0].id : null
+  value       = try(aws_wafv2_web_acl.main[0].id, null)
 }
 
 output "waf_web_acl_name" {
   description = "Name of the WAF Web ACL"
-  value       = var.enable_waf ? aws_wafv2_web_acl.main[0].name : null
+  value       = try(aws_wafv2_web_acl.main[0].name, null)
 }
 
 output "waf_log_group_name" {
   description = "CloudWatch log group name for WAF logs"
-  value       = var.enable_waf ? aws_cloudwatch_log_group.waf[0].name : null
+  value       = try(aws_cloudwatch_log_group.waf[0].name, null)
 }
 
 output "waf_log_group_arn" {
   description = "CloudWatch log group ARN for WAF logs"
-  value       = var.enable_waf ? aws_cloudwatch_log_group.waf[0].arn : null
+  value       = try(aws_cloudwatch_log_group.waf[0].arn, null)
 }
 
 output "waf_association_id" {
   description = "ID of the WAF Web ACL association with ALB"
-  value       = var.enable_waf ? aws_wafv2_web_acl_association.alb[0].id : null
+  value       = try(aws_wafv2_web_acl_association.alb[0].id, null)
 }
 
 output "vpc_flow_logs_id" {
